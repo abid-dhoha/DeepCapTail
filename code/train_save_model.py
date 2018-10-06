6fcae0f4bc507d2a24c7882f6a17dec16aa648b5
Dhoha Abid <abid.dhoha@gmail.com>
2018-10-06 16:16:40 -0400

    change the way to save deep learning model

--- code/train_save_model.py
+++ code/train_save_model.py
@@ -3,17 +3,18 @@ This model codes for the capsid and tail model: train and save modules
 """
 
 
-def train_model(df_data, s_label):
+def train_model(df_data, s_label, model_name):
     from code.capsid_tail_deep_models import capsid_model, tail_model
     from numpy import size
 
-    capsid_model = capsid_model(size(df_data, 1))
-    capsid_model.fit(df_data, s_label, epochs=150, batch_size=10, verbose=1)
+    if model_name == 'capsid':
+        model = capsid_model(size(df_data, 1))
+        model.fit(df_data, s_label, epochs=150, batch_size=10, verbose=0)
+    else:
+        model = tail_model(size(df_data, 1))
+        model.fit(df_data, s_label, epochs=150, batch_size=10, verbose=0)
 
-    tail_model = tail_model(size(df_data, 1))
-    tail_model.fit(df_data, s_label, epochs=150, batch_size=10, verbose=1)
-
-    return capsid_model, tail_model
+    return model
 
 
 def main():
@@ -22,12 +23,15 @@ def main():
     from os import makedirs
     from argparse import ArgumentParser
     from sklearn.externals.joblib import dump
+    import pickle
 
     parser = ArgumentParser()
-    parser.add('--p_lp_fasta', '-p_lp_fasta', help='path of the file that has the list of paths of fasta files '
-                                                   'that you intend to train')
-    parser.add('--p_l_label', '-p_l_label', help='path of the file that has the list of labels')
-    parser.add('--p_output', '-p_output', help='path of the output path where the constructed models will be stored')
+    parser.add_argument('--p_lp_fasta', '-p_lp_fasta', help='path of the file that has the list of paths of fasta'
+                                                            ' files that you intend to train')
+    parser.add_argument('--p_l_label', '-p_l_label', help='path of the file that has the list of labels')
+    parser.add_argument('--model_name', '-model_name', help='capsid or tail which is the name of the model')
+    parser.add_argument('--p_output', '-p_output', help='path of the output path where the constructed models '
+                                                        'will be stored')
 
     args = parser.parse_args()
     # build df_learn, which the learning matrix
@@ -36,14 +40,22 @@ def main():
         , l_label=[int(label) for label in open(args.p_l_label).read().splitlines()]
     )
     # train capsid and tail models
-    capsid_model, tail_model = train_model(df_data, s_label)
+    model = train_model(df_data, s_label, args.model_name)
     # create the output directory if doesn't exist
-    if exists(args.p_output):
-        makedirs(args.p_ouput)
+    #if exists(args.p_output):
+    #    makedirs(args.p_ouput)
     # save the capsid and tail models
-    dump(capsid_model, join(args.p_output, 'capsid.model'))
-    dump(tail_model, join(args.p_output, 'tail.model'))
+    # pickle.dump(model, open(args.p_output, 'wb'))
+
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(args.p_output, "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights('.'.join([args.p_output, 'h5']))
+    print("Saved model to disk")
+    # https://machinelearningmastery.com/save-load-keras-deep-learning-models/
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     main()
